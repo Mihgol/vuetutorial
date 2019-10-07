2c47b9c7c71453b2f4dfd0b81d3e0f69bffe16e7
mihgol <mihgol@wp.pl>
2019-10-07 17:54:53 +0200

    Add basic hero image view

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,28 +1,19 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input 
-        id="search" 
-        name="search" 
-        v-model="searchValue"
-        @input="handleInput"
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p> {{ item.data[0].description }}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import debounce from 'lodash.debounce'
+import Claim from '@/components/Claim.vue'
+import SearchInput from '@/components/SearchInput.vue'
 const API = 'https://images-api.nasa.gov/search'
 export default {
   name: 'Search',
+  components: { Claim, SearchInput },
   data() {
     return {
       searchValue: '',
@@ -46,27 +37,19 @@ export default {
 
 <style lang="scss" scoped>
   .wrapper {
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    padding: 30px;
     display: flex;
+    justify-content: center;
     flex-direction: column;
     align-items: center;
-    margin: 0;
-    padding: 30px;
-    width: 100%;
+    background-image: url('../assets/hero.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0%;
   }
 
-  .search {
-    display: flex;
-    flex-direction: column;
-    width: 250px;
-
-    label {
-      font-family: Monstserrat, sans-serif
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
-  }
+ 
 </style>
